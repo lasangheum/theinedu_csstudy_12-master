dd4242cee0efdc1fc06a8654e49a5383541dfd98
La Sang Heum <fktkdgma@gmail.com>
2024-08-09 09:58:54 +0900

    초기 커밋.

---
+++ Ch01Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch01Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 200;
+            int a2 = 100;
+
+            Console.WriteLine("a1 + a2 = " + (a1 + a2));
+            Console.WriteLine("a1 - a2 = " + (a1 - a2));
+            Console.WriteLine("a1 * a2 = " + (a1 * a2));
+            Console.WriteLine("a1 / a2 = " + (a1 / a2));
+            Console.WriteLine("a1 % a2 = " + (a1 % a2));
+
+
+        }
+    }
+}
